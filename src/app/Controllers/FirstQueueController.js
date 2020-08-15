0b5c9e87f09e9c22987ad1ccbf0e0b5d03c7a506
PedroPaduelo <pedropaduelo@gmail.com>
2020-08-15 17:33:47 +0000

    .

--- src/app/Controllers/FirstQueueController.js
+++ src/app/Controllers/FirstQueueController.js
@@ -1,16 +1,27 @@
 const crypto = require('crypto');
-const connection = require('../../database/connection')
+const db2 = require('../../database/connection')
 
 
 module.exports = {
     
     async Index(request, response){
+        
+        console.log("01 - Inicio da Rotina de dados!!!")
+
+        console.log("02 - Inicio busca bases no bamco de dados!!!")
+
+        const tbl_resultado = await db2('SORTEIOS');
+        const tbl_fechamento = await db2('FECHAMENTO_4_CSV');
+
+        console.log("03 - Fim busca bases no bamco de dados!!!")
+
+
+
+
+
+
 
         
-        const users = await connection('users').select('*');
-        return response.json(users)
-
-
-
+        return response.json("users")
     }
 };
---
+++ src/app/jobs/RegistroImport.js
@@ -0,0 +1,9 @@
+const db2 = require('../../database/connection');
+
+
+export default {
+    key: 'ImportInLot',
+    async handle(base_final_11)  {
+        console.log(base_final_11);
+    },
+};
